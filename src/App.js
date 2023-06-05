e1548f0c55aa0191f9319c1a8fcb1512367505f6
Basel Serhan <baselserhan1993@gmail.com>
2023-06-05 13:13:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses: [{ name: "HTML" }, { name: "CSS" }, { name: "jQuery" }],
+    current: "",
+  };
+
+  // UpdateCourse
+  updateCourse = (e) => {
+    this.setState({
+      current: e.target.value,
+    });
+  };
+
+  // AddCourse
+  addCourse = (e) => {
+    e.preventDefault();
+    // const { courses, current } = this.state;
+    // this.setState({
+    //   courses: [...courses, { name: current }],
+    //   current: ''
+    // });
+    let current = this.state.current;
+    let courses = this.state.courses;
+    if (current !== "") {
+      courses.push({ name: current });
+      this.setState({
+        courses,
+        current: "",
+      });
+    }
+  };
+
+  // DeleteCourse
+  deleteCourse = (index) => {
+    //  const { courses } = this.state;
+    //   this.setState({
+    //     courses: courses.filter((course, i) => {
+    //       return i !== index;
+    //     })
+    //   });
+    let courses = this.state.courses;
+    courses.splice(index, 1);
+    this.setState({
+      courses,
+    });
+  };
+
+  //editCourse
+  editCourse = (index, value) => {
+    let courses = this.state.courses;
+    let course = courses[index];
+    course["name"] = value;
+    this.setState({
+      courses,
+    });
+  };
+
+  render() {
+    const { courses } = this.state;
+    let length = courses.length;
+    const courseList = length? (courses.map((course, index) => {
+      return (
+        <CourseList
+          details={course}
+          key={index}
+          index={index}
+          deleteCourse={this.deleteCourse}
+          editCourse={this.editCourse}
+        />
+      );
+    })): (<p>There is no courses to show</p>);
+    return (
+      <section className="App">
+        <h2>Add Course</h2>
+        <CourseForm
+          current={this.state.current}
+          updateCourse={this.updateCourse}
+          addCourse={this.addCourse}
+        />
+        <ul>{courseList}</ul>
+      </section>
+    );
+  }
 }
 
 export default App;
